aa6c9cac1843e584b282912550bc0f4abafd94f6
Roger <josenandopsousa@gmail.com>
2026-09-25 00:33:00 -0300

    fix: mover arquivos de rotas para dentro da pasta src

--- profile.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', (req, res) => {
-  return res.status(200).json([{ id: 1, name: "Perfil de Teste" }]);
-});
-
-export default router;
